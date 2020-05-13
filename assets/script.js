c8b4f3d01cc105c509547a5b5fba5a003c820117
megan-pg <megan.pardygokcu@unh.edu>
2020-05-13 18:04:16 -0400

    appending ojects from the API to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,34 @@
 var todayDate = moment().format('');
 
-$.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",US&units=imperial&APPID=abcd9257d5733a460d1691720d4f7b99",
-    method: "GET"
-}).then(function (response) {
-    // console.log(response);
-    var iconCode = response.weather[0].icon;
-    var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
-})
+function searchCities(cityName) {
+    var apiKey = "8c42537d5174398ca75f180d9d409dc2";
+
+    //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+
+
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",US&units=imperial&APPID=8c42537d5174398ca75f180d9d409dc2",
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var iconCode = response.weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
+        $("#temperature").text("Temperature: " + response.main.temp);
+        //console.log(response.main.temp);
+        $("#humidity").text("Humidity: " + response.main.humidity);
+        $("#windSpeed").text("Windspeed: " + response.wind.speed);
+        $(".card-header").text(cityName)
+    })
+
+    //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=8c42537d5174398ca75f180d9d409dc2",
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var iconCode = response.weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
+    })
+}
+
+console.log(searchCities("New York City"));
